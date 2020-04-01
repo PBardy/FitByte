c61b698621fb32ecec5b0ce6b193b11b85b64410
Philip <pbardy2000@gmail.com>
2020-04-01 15:44:44 +0100

    latest changes

--- main.py
+++ main.py
@@ -9,13 +9,65 @@ DB_USER_NAME = "root"
 DB_PASSWORD = ""
 DB_DATABASE = "fitbyte"
 
-MAX_USERNAME_LENGTH = 32
-MAX_PASSWORD_LENGTH = 72
-MIN_PASSWORD_LENGTH = 6
 MAX_NAME_LENGTH = 35
 
 POUNDS_TO_KILO_RATIO = 1/2.205
 CM_TO_INCHES_RATIO = 1/2.54
+KCAL_TO_KJ_RATIO = 1/4.184
+
+"""
+ Represents the account stored in the database.
+"""
+class Account:
+
+  MAX_USERNAME_LENGTH = 32
+  MAX_PASSWORD_LENGTH = 72
+  MIN_PASSWORD_LENGTH = 6
+
+  def __init__(self):
+    self.__username = self.__get_username()
+    self.__password = self.__get_password()
+  
+  def __get_username(self):
+    while True:
+      username = str(input("Username: "))
+      if len(username) > 0:
+        if len(username) < Account.MAX_USERNAME_LENGTH:
+          return username
+        else:
+          print()
+          print("Username is too long")
+      else:
+        print()
+        print("Username cannot be empty")
+
+  def __get_password(self):
+    while True:
+      password1 = str(input("Password: "))
+      password2 = str(input("Repeat password: "))
+      if password1 == password2:
+        if len(password1) > Account.MIN_PASSWORD_LENGTH:
+          if len(password1) < Account.MAX_PASSWORD_LENGTH:
+            return password1
+          else:
+            print()
+            print("Password is too long. Passwords must be shorter than %s characters." % Account.MAX_PASSWORD_LENGTH)
+        else:
+          print()
+          print("Password must be longer than %s characters." % Account.MIN_PASSWORD_LENGTH)
+      else:
+        print()
+        print("Passwords do not match.")
+
+  def get_id(self):
+    return self.__id
+  
+  def get_username(self):
+    return self.__username
+
+  def get_password(self):
+    return self.__password
+
 
 """
   Connects to the database.
@@ -67,24 +119,26 @@ def create_profile(id, first_name, last_name, dob, weight, height, sex, activity
 
   @return - The account id (for identification)
 """
-def create_account(username, password):
+def create_account(account):
 
     db = mysqli()
     cursor = db.cursor()
 
     salt = bcrypt.gensalt()
-    password = password.encode("utf-8")
+    password = account.get_password().encode("utf-8")
     hash = bcrypt.hashpw(password, salt)
 
     salt = salt.decode("utf-8")
     hash = hash.decode("utf-8")
+
+    print(salt)
 
     sql = """
       INSERT INTO accounts (username, password_salt, password_hash, date_created) 
       VALUES (%s, %s, %s, CURDATE())
     """
 
-    values = (username, salt, hash)
+    values = (account.get_username(), salt, hash)
 
     cursor.execute(sql, values)
 
@@ -286,6 +340,23 @@ def display_all_badges():
   print()
   print("All achievable badges")
 
+  # Create a database connection
+  db = mysqli()
+  cursor = db.cursor()
+
+  query = "SELECT description FROM badges"
+
+  cursor.execute(query)
+  results = cursor.fetchall()
+  db.close()
+
+  if len(results) > 0:
+    for result in results:
+      print(result)
+  else:
+    print("ERROR - Could not find badges")
+    print()
+
 
 """
   Displays all badges achieved by the specified user.
@@ -336,6 +407,52 @@ def add_user_goal(id):
   print("Add a new goal")
   print()
 
+  metric = ""
+  valid_metric = False
+
+  while not valid_metric:
+    
+    print()
+    print("I want to track:")
+    print()
+    print("1. Energy intake (calories)")
+    print("2. Fat intake")
+    print("3. Fibre intake")
+    print("4. Protein intake")
+    print("5. Salt intake")
+    print("6. Sugar intake")
+    print("7. Weight")
+    print("8. None of the above (return to menu)")
+    print()
+
+    choice = get_menu_choice(8)
+
+    if choice > 0:
+      valid_metric = True
+      if choice == 1:
+        metric = "energy_intake"
+        continue
+      if choice == 2:
+        metric = "fat_intake"
+        continue
+      if choice == 3:
+        metric = "fibre_intake"
+        continue
+      if choice == 4:
+        metric = "protein_intake"
+        continue
+      if choice == 5:
+        metric = "salt_intake"
+        continue
+      if choice == 6:
+        metric = "sugar_intake"
+        continue
+      if choice == 7:
+        metric = "weight"
+        continue
+      if choice == 8:
+        return
+    
 
 """
   Allows a user to modify a current goal.
@@ -400,8 +517,8 @@ def edit_account_menu(id):
 
 """
   Displays the menu for editing the profile details. From this
-  menu the user can change the values stored about them in their
-  profile. 
+  menu the user can change the values stored about them in 
+  their profile. 
 
   @param id - The unique identifier for the user
 """
@@ -409,11 +526,317 @@ def edit_profile_menu(id):
   
   print()
   print("Edit profile information")
+  print("Press enter to keep the same")
 
   complete = False
 
   while not complete:
-    pass
+    
+    print()
+
+    # Prompt user to enter their name
+    first_name = str(input("First name: "))
+    last_name = str(input("Last name: "))
+
+    # Ensure first name and last name are not empty
+    if len(first_name) <= 0 or len(last_name) <= 0:
+      print("Names cannot be empty")
+      continue
+
+    # Ensure names are not over database limit of 35 characters
+    if len(first_name) > MAX_NAME_LENGTH or len(last_name) > MAX_NAME_LENGTH:
+      print("Names must not be longer than %d", MAX_NAME_LENGTH)
+      continue
+
+    # Prompt user to enter their DOB (so we can calculate age)
+    dob = get_date("Date of birth: ")
+
+    # Prompt user to enter their current weight
+    current_weight_units = str(input("Current weight units (kg, lb): "))
+
+    # Ensure only valid units are accepted (so we can convert to standard units)
+    if current_weight_units.lower() == "kg" or current_weight_units.lower() == "lb":
+      current_weight_magnitude = get_float("Current weight (magnitude): ", 0, 1000)
+      if current_weight_magnitude != None:
+        # Convert pounds to kilograms
+        if current_weight_units.lower() == "lb":
+          current_weight_magnitude *= POUNDS_TO_KILO_RATIO 
+      else:
+        continue
+    else:
+      print("Weight units must be either kg or lb")
+      continue
+
+    # Prompt user for their height (stored in cm)#
+    height_units = str(input("Height units (cm, inches)"))
+
+    # Ensure entered units are valid
+    if height_units.lower() == "cm" or height_units.lower() == "inches":
+      height = get_float("Height (magnitude): ", 0, 1000)
+      if height != None:
+        # Convert feet to cm
+        if height_units.lower() == "inches":
+          height *= CM_TO_INCHES_RATIO
+      else:
+        continue
+    else:
+      print("Height units must be either cm or ft")
+      continue
+
+    # Prompt user for their sex (this is used for calculating BMI)
+    sex = str(input("Sex (M/F): "))
+
+    # Ensure entered sex is valid
+    if not(sex.lower() == "m" or sex.lower() == "f"):
+      print("Sex must be either m or f")
+      continue
+
+    # Prompt user for their activity rating (1-5)
+    print("Describe your activity: ")
+    print("1. Extremely inactive")
+    print("2. Sedentary (no excercise)")
+    print("3. Moderately active (some excercise)")
+    print("4. Vigorously active (lots of excersie)")
+    print("5. Extremely active (i.e. an athelete)")
+    print()
+
+    activity_rating = get_menu_choice(5)
+
+    # Ensure valid menu option has been selected
+    if not(activity_rating > 0):
+      print("Enter a menu choice between 1 and 5")
+      continue
+
+
+"""
+  Displays the user's data in table format. For large datasets
+  this information is displayed in multi-page format. The user
+  can choose which page they want displayed.
+
+  @param id - The unique identifier for the user
+"""
+def view_all_data_menu(id):
+  pass
+
+
+"""
+  Displays the user's bmr using 3 different equations.
+
+  @param id - The unique identifier for the user
+"""
+def view_my_bmr(id):
+  pass
+
+
+"""
+  Display a menu which allows the user to generate a graph of 
+  their data. These graphs can be produced for each measured 
+  catergory (e.g. weight, calories, salt intake... ), and can 
+  display this information in four different time scales: 
+  weekly, bi-weekly, monthly and yearly.
+
+  @param metric - The table from which data is to be collected.
+  @param timescale - Specifies how data is grouped (e.g. Weekly)
+"""
+def create_graph(metric, timescale):
+  pass
+
+
+"""
+  Display a menu which allows the user to generate a graph of 
+  their data. These graphs can be produced for each measured 
+  catergory (e.g. weight, calories, salt intake... ), and can 
+  display this information in four different time scales: 
+  weekly, bi-weekly, monthly and yearly.
+
+  @param id - The unique identifier for the user
+"""
+def view_my_graphs_menu(id):
+  
+  print()
+  print("View my data as graphs")
+
+  metric = ""
+  valid_metric = False
+
+  while not valid_metric:
+    
+    print()
+    print("Graph:")
+    print()
+    print("1. My energy intake (calories)")
+    print("2. My fat intake")
+    print("3. My fibre intake")
+    print("4. My protein intake")
+    print("5. My salt intake")
+    print("6. My sugar intake")
+    print("7. My weight")
+    print("8. None of the above (return to menu)")
+    print()
+
+    choice = get_menu_choice(8)
+
+    if choice > 0:
+      valid_metric = True
+      if choice == 1:
+        metric = "energy_intake"
+        continue
+      if choice == 2:
+        metric = "fat_intake"
+        continue
+      if choice == 3:
+        metric = "fibre_intake"
+        continue
+      if choice == 4:
+        metric = "protein_intake"
+        continue
+      if choice == 5:
+        metric = "salt_intake"
+        continue
+      if choice == 6:
+        metric = "sugar_intake"
+        continue
+      if choice == 7:
+        metric = "weight"
+        continue
+      if choice == 8:
+        return
+
+  timescale = ""
+  valid_timescale = False
+
+  while not valid_timescale:
+    
+    print()
+    print("Display graph with a")
+    print()
+    print("1. Weekly breakdown")
+    print("2. Bi-weekly breakdown")
+    print("3. Monthly breakdown")
+    print("4. Yearly breakdown")
+
+    choice = get_menu_choice(4)
+
+    if choice > 0:
+      valid_timescale = True
+      create_graph(metric, choice)
+
+
+"""
+  Allows the user add data from their own tracking.
+
+  @param id - The unique identifier for the user
+"""
+def add_informatics_data(id):
+  
+  print()
+  print("Add informatics data")
+
+  valid_entry = False
+
+  while not valid_entry:
+    
+    print()
+    print("1. Add calorie information")
+    print("2. Add fat information")
+    print("3. Add fibre information")
+    print("4. Add protein information")
+    print("5. Add salt information")
+    print("6. Add sugar information")
+    print("7. Add weight information")
+    print("8. Return")
+
+    choice = get_menu_choice(8)
+
+    if choice > 0:
+
+      # Attempt to get the amount of calories intaken
+      if choice == 1:
+        table = "enery_intake"
+        print("How many calories did you intake?")
+        units = str(input("Units (kcal or kJ"))
+        if units.lower() == "kcal" or units.lower() == "kJ":
+          value = get_float("Value: ", 0, 10000)
+          if value != None:
+            if units.lower() == "kcal":
+              value *= KCAL_TO_KJ_RATIO
+          else:
+            continue
+        else:
+          print("Invalid units")
+          continue
+
+      # Attempt to get the amount of fat intaken
+      if choice == 2:
+        table = "fat_intake"
+        print("How many grams of fat did you intake?")
+        value = get_float("Value: ", 0, 10000)
+        if value == None:
+          continue
+
+      # Attempt to get the amount of fibre intaken
+      if choice == 3:
+        table = "fibre_intake"
+        print("How many grams of fibre did you intake?")
+        value = get_float("Value: ", 0, 10000)
+        if value == None:
+          continue
+
+      # Attempt to get the amount of protein intaken
+      if choice == 4:
+        table = "protein_intake"
+        print("How many grams of protein did you intake?")
+        value = get_float("Value: ", 0, 10000)
+        if value == None:
+          continue
+
+      # Attempt to get the amount of salt intake
+      if choice == 5:
+        table = "salt_intake"
+        print("How many grams of salt did you intake?")
+        value = get_float("Value: ", 0, 10000)
+        if value == None:
+          continue
+
+      # Attempt to get the amount of sugar intake
+      if choice == 6:
+        table = "sugar_intake"
+        print("How many grams of sugar did you intake?")
+        value = get_float("Value: ", 0, 10000)
+        if value == None:
+          continue
+
+      if choice == 7:
+        table = "weight_intake"
+        print()
+        print("What was your measured weight?")
+        units = str(input("Units (kg or lb): "))
+        if units.lower() == "kg" or units.lower() == "lb":
+          value = get_float("Value: ", 0, 1000)
+          if value != None:
+            if units.lower() == "lb":
+              value *= POUNDS_TO_KILO_RATIO
+        else:
+          print("Invalid units")
+          continue          
+      
+      print()
+      date = get_date("When did you make this measurement?")
+      break
+  
+  db = mysqli()
+  cursor = db.cursor()
+
+  sql = """
+    INSERT INTO %s (account_id, date, value) 
+    VALUES (%s, %s, %s)
+  """
+
+  values = (table, id, date, value)
+
+  cursor.execute(sql, values)
+  db.commit()
+  db.close()
 
 
 """
@@ -432,7 +855,32 @@ def my_personal_data_menu(id):
   complete = False
 
   while not complete:
-    pass
+    
+    print()
+    print("1. View all data (as table)")
+    print("2. View my BMR")
+    print("3. View my graphs")
+    print("4. Add data")
+    print("5. Return")
+    print()
+
+    choice = get_menu_choice(5)
+
+    if choice > 0:
+      if choice == 1:
+        view_all_data_menu(id)
+        continue
+      if choice == 2:
+        view_my_bmr(id)
+        continue
+      if choice == 3:
+        view_my_graphs_menu(id)
+        continue
+      if choice == 4:
+        add_informatics_data(id)
+        continue
+      if choice == 5:
+        return
 
 
 """
@@ -513,6 +961,7 @@ def main_menu(id):
     print("3. My personal informatics data")
     print("4. My goals")
     print("5. Exit")
+    print()
 
     choice = get_menu_choice(2)
 
@@ -531,6 +980,7 @@ def main_menu(id):
         continue
       if choice == 5:
         session_active = False
+
 
 """
   Displays the login menu. This prompts the user for a 
@@ -670,35 +1120,14 @@ def create_account_menu():
   print()
   print("Create your account.")
 
-  created = False
+  successful = False
 
-  while not created:
-    print()
-    username = str(input("Username: "))
-    password = str(input("Password: "))
-    repeated = str(input("Repeat password:"))
+  while not successful:
 
-    # Prevent error when storing the username in the database
-    if len(username) > MAX_USERNAME_LENGTH:
-      print("Usernames are limited to %s characters", MAX_USERNAME_LENGTH)
-      continue
-
-    # Prevent an error when hashing with bcrypt
-    if len(password) > MAX_PASSWORD_LENGTH:
-      print("Passwords are limited to %s characters", MAX_PASSWORD_LENGTH)
-      continue
-
-    if len(password) < MIN_PASSWORD_LENGTH:
-      print("Passwords must be %s characters in length", MIN_PASSWORD_LENGTH)
-      continue
-
-    # Ensure use knows the password they entered
-    if password != repeated:
-      print("Passwords do not match")
-      continue
+    account = Account()
 
     # At this point all details should be valid for database entry
-    account_id = create_account(username, password)
+    account_id = create_account(account)
 
     # If not account id is returned then the entry was not created
     if type(account_id) != int:
